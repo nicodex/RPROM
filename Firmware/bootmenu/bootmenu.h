a935d0a698b2c172e1d204a27df863f4ff1f4a6c
Nico Bendlin <nico@nicode.net>
2026-04-28 13:13:12 +0200

    bootmenu: add firmware interface

---
+++ Firmware/bootmenu/bootmenu.h
@@ -0,0 +1,163 @@
+#ifndef RPROM_FIRMWARE_BOOTMENU_H
+#define RPROM_FIRMWARE_BOOTMENU_H
+
+#include <stdint.h>
+
+/* ROM interface is word-based (no A0) */
+#define RPBM_ADDR_SHIFT 1u
+
+/* RPROM firmware page size (256 bytes) */
+#define RPBM_PAGE_BITS 8u
+#define RPBM_PAGE_SIZE (1u << RPBM_PAGE_BITS)
+
+/*
+ * RPROM firmware is expected to set it after loading the bootmenu
+ * (used by the bootmenu to find itself when run from $00/overlay)
+ */
+#define RPBM_FWSERIAL_ADDR 0x00005Cu /* VEC_RESV23 (23*4) */
+#define RPBM_FWSERIAL_WORD (RPBM_FWSERIAL_ADDR >> RPBM_ADDR_SHIFT)
+
+/*
+ * Reading this magic address/word sends the next read word offset
+ * as command/data to the firmware -- since the bootmenu code runs
+ * in the ROM area -- the only practical read-only CPU instruction
+ * available is "CMPM.W (Ay)+,(Ax)+ ; [6].r.r.p", which allows two
+ * addresses to be read directly one after the other (without the
+ * CPU instruction pipeline accessing other addresses in between).
+ * A single-value magic address sequence is generally acceptable,
+ * since we have full control over the code in the bootmenu mode;
+ * however, we do not have exclusive control over the system, and
+ * the protocol must be protected from accidental commands caused
+ * by read accesses from external hardware by the protocol design.
+ */
+#define RPBM_FWMAGIC1_ADDR 0x0000ECu /* VEC_RESV59 (59*4) */
+#define RPBM_FWMAGIC1_WORD (RPBM_FWMAGIC1_ADDR >> RPBM_ADDR_SHIFT)
+
+/* RPROM firmware data transfer buffer (second bootmenu page) */
+#define RPBM_FWBUFFER_ADDR 0x000100u /* VEC_USER[0-63] (64*4-127*4) */
+#define RPBM_FWBUFFER_WORD (RPBM_FWBUFFER_ADDR >> RPBM_ADDR_SHIFT)
+#define RPBM_FWBUFFER_SIZE RPBM_PAGE_SIZE
+
+/* RPROM firmware command/data transfer status (volatile) */
+#define RPBM_FWSTATUS_ADDR 0x000200u /* VEC_USER[64] (128*4) */
+#define RPBM_FWSTATUS_WORD (RPBM_FWSTATUS_ADDR >> RPBM_ADDR_SHIFT)
+#define RPBM_FWSTATUSB_BUSY (7u+24u) /* DQ7 */
+#define RPBM_FWSTATUSF_BUSY (1u << RPBM_FWSTATUSB_BUSY)
+#define RPBM_FWSTATUSB_FAIL (5u+24u) /* DQ5 */
+#define RPBM_FWSTATUSF_FAIL (1u << RPBM_FWSTATUSB_FAIL)
+
+/*
+ * RPROM firmware bootmenu command/data word format:
+ *
+ *	| data | value                             | ROM address access range |
+ *	| ---: | :-------------------------------- | -----------------------: |
+ *	| `=1` | `0bFEDCBA9876543210`              |     (0x020000, 0x03FFFE) |
+ *
+ *	| data | func |  command | param           | ROM address access range |
+ *	| ---: | :--- |--------: | :-------------- | -----------------------: |
+ *	| `=0` | `=1` | `0bEDCB` | `0bA9876543210` |     (0x010000, 0x01FFFE) |
+ *
+ * NOTE: Only 17 bits (128K words), due to F0 address decoder /ROMEN.
+ */
+#define RPBM_FWWORD_BITS 17u /* 18 - RPBM_ADDR_SHIFT */
+#define RPBM_FWWORD_MASK ((1u << RPBM_FWWORD_BITS) - 1u)
+/* if data == 1: remaining bits are the transferred data value */
+#define RPBM_FWWORDB_DATA 16u /* RPBM_FWWORD_BITS - 1 */
+#define RPBM_FWWORDF_DATA (1u << RPBM_FWWORDB_DATA)
+#define RPBM_FWWORD_VALUE_BITS 16u
+#define RPBM_FWWORD_VALUE_MASK (1u << RPBM_FWWORD_VALUE_BITS)
+/* if data == 0 and func == 1: command and param bits (4 + 11) */
+#define RPBM_FWWORDB_FUNC 15u /* RPBM_FWWORDB_DATA - 1 */
+#define RPBM_FWWORDF_FUNC (1u << RPBM_FWWORDB_FUNC)
+#define RPBM_FWWORD_CMDID_BITS 4u
+#define RPBM_FWWORD_CMDID_MASK ((1u << RPBM_FWWORD_CMDID_BITS) - 1u)
+#define RPBM_FWWORD_CMDID_BASE (RPBM_FWWORDB_FUNC - RPBM_FWWORD_CMDID_BITS)
+#define RPBM_FWWORD_PARAM_BITS 11u /* enough to address 2048 pages/slot */
+#define RPBM_FWWORD_PARAM_MASK ((1u << RPBM_FWWORD_PARAM_BITS) - 1u)
+
+/* RPROM firmware bootmenu command IDs */
+enum BootMenuCmdID {
+	RPBM_CMDID_FIRMWAREINFO, /* 0b0000 */
+	RPBM_CMDID_BOOTMENUINFO, /* 0b0001 */
+	RPBM_CMDID_SLOT_TO_KICK, /* 0b0010 */
+	RPBM_CMDID_JUMP_TO_KICK, /* 0b0011 */
+	RPBM_CMDID_RESERVED_04,  /* 0b0100 */
+	RPBM_CMDID_RESERVED_05,  /* 0b0101 */
+	RPBM_CMDID_RESERVED_06,  /* 0b0110 */
+	RPBM_CMDID_RESERVED_07,  /* 0b0111 */
+	RPBM_CMDID_RESERVED_08,  /* 0b1000 */
+	RPBM_CMDID_RESERVED_09,  /* 0b1001 */
+	RPBM_CMDID_RESERVED_10,  /* 0b1010 */
+	RPBM_CMDID_RESERVED_11,  /* 0b1011 */
+	RPBM_CMDID_RESERVED_12,  /* 0b1100 */
+	RPBM_CMDID_RESERVED_13,  /* 0b1101 */
+	RPBM_CMDID_RESERVED_14,  /* 0b1110 */
+	RPBM_CMDID_EXTENDED_CMD  /* 0b1111 */
+};
+
+/*****************************************************************************
+ *
+ *	RPBM_CMDID_FIRMWAREINFO
+ *	param: reserved (1)
+ *
+ * The firmware writes a FirmwareInfo struct into the buffer page.
+ */
+#define RPBM_FIRMWAREINFO_PARAM 1u
+struct FirmwareInfo { /* TODO: move this into protocol header */
+  	uint32_t Magic;    /* 00: RPFW_FIRMWAREINFO_MAGIC */
+#define RPFW_FIRMWAREINFO_MAGIC 'RPRM'
+  	uint8_t  InfoSize; /* 04: sizeof FirmwareInfo & 0xFF (0 = 256) */
+  	uint8_t  FwMajor;  /* 05: firmware major version (0 = develop) */
+  	uint8_t  FwMinor;  /* 06: firmware minor version */
+  	uint8_t  FwPatch;  /* 07: firmware patch version */
+  	uint8_t  FlashMB;  /* 08: Flash size in MB (4MB = 7 slots) */
+  	uint8_t  BootSlot; /* 09: default boot slot from stored config */
+  	uint16_t BootConf; /* 0A: see struct BootMenuInfo.BootConf */
+  	uint8_t  KickSlot; /* 0C: slot loaded in Kickstart SRAM memory */
+  	uint8_t  WorkSlot; /* 0D: current Flash slot (page read/write) */
+  	uint16_t reserved; /* 0E: reserved/alignment (0) */
+};	                   /* 10: sizeof FirmwareInfo */
+
+/*****************************************************************************
+ *
+ *	RPBM_CMDID_BOOTMENUINFO
+ *	param: reserved (1)
+ *
+ * The firmware writes a BootMenuInfo struct into the buffer page.
+ */
+#define RPBM_BOOTMENUINFO_PARAM 1u
+struct BootMenuSlotInfo {
+  	uint32_t KickInfo; /* 00: TODO: compressed Kickstart details */
+  	uint32_t ResetPC;  /* 04: VEC_RESETPC value */
+};	                   /* 08: sizeof BootMenuSlotInfo */
+struct BootMenuInfo {
+  	uint8_t  SlotCount; /* 00: 1-31 (4MB = 7 slots) */
+  	uint8_t  BootSlot;  /* 01: see struct FirmwareInfo.BootSlot */
+  	uint16_t BootConf;  /* 02: TODO: bootmenu UI and/or control flags */
+  	uint32_t SlotValid; /* 04: bit 0 reserved, bit 1-31 slot valid */
+  	struct BootMenuSlotInfo SlotInfo[31]; /* 08: (16MB = 31 slots) */
+};	                   /* 100: sizeof BootMenuInfo == RPBM_PAGE_SIZE */
+
+/*****************************************************************************
+ *
+ *	RPBM_CMDID_SLOT_TO_KICK
+ *	param: <slot_index> (0 = firmware/config)
+ *
+ * The firmware copies a slot from Flash storage into Kickstart SRAM memory.
+ * - firmware sets struct FirmwareInfo.KickSlot = <slot_index>
+ */
+#define RPBM_FIRMWARE_TO_KICK 0u /* including config storage (BootSlot) */
+
+/*****************************************************************************
+ *
+ *	RPBM_CMDID_JUMP_TO_KICK
+ *	param: reserved (0)
+ *
+ * The firmware exits bootmenu mode and switches to Kickstart SRAM memory.
+ * Point of no return -- the bootmenu is responsible to setup/prepare the
+ * system before sending this command, and has to immediately jump to the
+ * Kickstart (jump has to be run from the CPU instruction prefetch queue).
+ */
+#define RPBM_JUMP_TO_KICK_PARAM 0u
+
+#endif /* RPROM_FIRMWARE_BOOTMENU_H */
---
+++ Firmware/bootmenu/bootmenu.i
@@ -0,0 +1,159 @@
+	IFND	RPROM_FIRMWARE_BOOTMENU_I
+RPROM_FIRMWARE_BOOTMENU_I	SET 	1
+
+; ROM interface is word-based (no A0)
+RPBM_ADDR_SHIFT	EQU 	1
+
+; RPROM firmware page size (256 bytes)
+RPBM_PAGE_BITS	EQU 	8
+RPBM_PAGE_SIZE	EQU 	(1<<RPBM_PAGE_BITS)
+
+;
+; RPROM firmware is expected to set it after loading the bootmenu
+; (used by the bootmenu to find itself when run from $00/overlay)
+;
+RPBM_FWSERIAL_ADDR	EQU 	$00005C ; VEC_RESV23 (23*4)
+RPBM_FWSERIAL_WORD	EQU 	(RPBM_FWSERIAL_ADDR>>RPBM_ADDR_SHIFT)
+
+;
+; Reading this magic address/word sends the next read word offset
+; as command/data to the firmware -- since the bootmenu code runs
+; in the ROM area -- the only practical read-only CPU instruction
+; available is "CMPM.W (Ay)+,(Ax)+ ; [6].r.r.p", which allows two
+; addresses to be read directly one after the other (without the
+; CPU instruction pipeline accessing other addresses in between).
+; A single-value magic address sequence is generally acceptable,
+; since we have full control over the code in the bootmenu mode;
+; however, we do not have exclusive control over the system, and
+; the protocol must be protected from accidental commands caused
+; by read accesses from external hardware by the protocol design.
+;
+RPBM_FWMAGIC1_ADDR	EQU 	$0000EC ; VEC_RESV59 (59*4)
+RPBM_FWMAGIC1_WORD	EQU 	(RPBM_FWMAGIC1_ADDR>>RPBM_ADDR_SHIFT)
+
+; RPROM firmware data transfer buffer (second bootmenu page)
+RPBM_FWBUFFER_ADDR	EQU 	$000100 ; VEC_USER[0-63] (64*4-127*4)
+RPBM_FWBUFFER_WORD	EQU 	(RPBM_FWBUFFER_ADDR>>RPBM_ADDR_SHIFT)
+RPBM_FWBUFFER_SIZE	EQU 	RPBM_PAGE_SIZE
+
+; RPROM firmware command/data transfer status (volatile)
+RPBM_FWSTATUS_ADDR	EQU 	$000200 ; VEC_USER[64] (128*4)
+RPBM_FWSTATUS_WORD	EQU 	(RPBM_FWSTATUS_ADDR>>RPBM_ADDR_SHIFT)
+RPBM_FWSTATUSB_BUSY	EQU 	7+24 ; DQ7
+RPBM_FWSTATUSF_BUSY	EQU 	(1<<RPBM_FWSTATUSB_BUSY)
+RPBM_FWSTATUSB_FAIL	EQU 	5+24 ; DQ5
+RPBM_FWSTATUSF_FAIL	EQU 	(1<<RPBM_FWSTATUSB_FAIL)
+
+;
+; RPROM firmware bootmenu command/data word format:
+;
+;	| data | value                           | ROM address access range |
+;	| ---: | :------------------------------ | -----------------------: |
+;	| `=1` | `%FEDCBA9876543210`             |       ($020000, $03FFFE) |
+;
+;	| data | func | command | param          | ROM address access range |
+;	| ---: | :--- |-------: | :------------- | -----------------------: |
+;	| `=0` | `=1` | `%EDCB` | `%A9876543210` |       ($010000, $01FFFE) |
+;
+; NOTE: Only 17 bits (128K words), due to F0 address decoder /ROMEN.
+;
+RPBM_FWWORD_BITS	EQU 	17 ; 18-RPBM_ADDR_SHIFT
+RPBM_FWWORD_MASK	EQU 	((1<<RPBM_FWWORD_BITS)-1)
+; if data == 1: remaining bits are the transferred data value
+RPBM_FWWORDB_DATA	EQU 	16 ; RPBM_FWWORD_BITS-1
+RPBM_FWWORDF_DATA	EQU 	(1<<RPBM_FWWORDB_DATA)
+RPBM_FWWORD_VALUE_BITS	EQU 	16
+RPBM_FWWORD_VALUE_MASK	EQU 	(1<<RPBM_FWWORD_VALUE_BITS)
+; if data == 0 and func == 1: command and param bits (4+11)
+RPBM_FWWORDB_FUNC	EQU 	15 ; RPBM_FWWORDB_DATA-1
+RPBM_FWWORDF_FUNC	EQU 	(1<<RPBM_FWWORDB_FUNC)
+RPBM_FWWORD_CMDID_BITS	EQU 	4
+RPBM_FWWORD_CMDID_MASK	EQU 	((1<<RPBM_FWWORD_CMDID_BITS)-1)
+RPBM_FWWORD_CMDID_BASE	EQU 	(RPBM_FWWORDB_FUNC-RPBM_FWWORD_CMDID_BITS)
+RPBM_FWWORD_PARAM_BITS	EQU 	11 ; enough to address 2048 pages/slot
+RPBM_FWWORD_PARAM_MASK	EQU 	((1<<RPBM_FWWORD_PARAM_BITS)-1)
+
+; RPROM firmware bootmenu command IDs
+RPBM_CMDID_FIRMWAREINFO	EQU 	%0000
+RPBM_CMDID_BOOTMENUINFO	EQU 	%0001
+RPBM_CMDID_SLOT_TO_KICK	EQU 	%0010
+RPBM_CMDID_JUMP_TO_KICK	EQU 	%0011
+RPBM_CMDID_RESERVED_04 	EQU 	%0100
+RPBM_CMDID_RESERVED_05 	EQU 	%0101
+RPBM_CMDID_RESERVED_06 	EQU 	%0110
+RPBM_CMDID_RESERVED_07 	EQU 	%0111
+RPBM_CMDID_RESERVED_08 	EQU 	%1000
+RPBM_CMDID_RESERVED_09 	EQU 	%1001
+RPBM_CMDID_RESERVED_10 	EQU 	%1010
+RPBM_CMDID_RESERVED_11 	EQU 	%1011
+RPBM_CMDID_RESERVED_12 	EQU 	%1100
+RPBM_CMDID_RESERVED_13 	EQU 	%1101
+RPBM_CMDID_RESERVED_14 	EQU 	%1110
+RPBM_CMDID_EXTENDED_CMD	EQU 	%1111
+
+;-----------------------------------------------------------------------------
+;
+;	RPBM_CMDID_FIRMWAREINFO
+;	param: reserved (1)
+;
+; The firmware writes a FirmwareInfo struct into the buffer page.
+;
+RPBM_FIRMWAREINFO_PARAM	EQU 	1
+; struct FirmwareInfo ;TODO: move this into protocol header
+rpfwi_Magic     	EQU 	$00 ;<.l> RPFW_FIRMWAREINFO_MAGIC
+RPFW_FIRMWAREINFO_MAGIC	EQU 	'RPRM'
+rpfwi_InfoSize  	EQU 	$04 ;<.b> rpfwi_SIZEOF (0 = 256)
+rpfwi_FwMajor   	EQU 	$05 ;<.b> firmware major version (0 = develop)
+rpfwi_FwMinor   	EQU 	$06 ;<.b> firmware minor version
+rpfwi_FwPatch   	EQU 	$07 ;<.b> firmware patch version
+rpfwi_FlashMB   	EQU 	$08 ;<.b> Flash size in MB (4MB = 7 slots)
+rpfwi_BootSlot  	EQU 	$09 ;<.b> default boot slot from stored config
+rpfwi_BootConf  	EQU 	$0A ;<.w> see rpbmi_BootConf
+rpfwi_KickSlot  	EQU 	$0C ;<.b> slot loaded in Kickstart SRAM memory
+rpfwi_WorkSlot  	EQU 	$0D ;<.b> current Flash slot (page read/write)
+rpfwi_reserved  	EQU 	$0E ;<.w> reserved/alignment (0)
+rpfwi_SIZEOF    	EQU 	$10&$FF
+
+;-----------------------------------------------------------------------------
+;
+;	RPBM_CMDID_BOOTMENUINFO
+;	param: reserved (1)
+;
+; The firmware writes a BootMenuInfo struct into the buffer page.
+;
+RPBM_BOOTMENUINFO_PARAM	EQU 	1
+; struct BootMenuSlotInfo
+rpbmsi_KickInfo	EQU 	$00 ;<.l> ;TODO: compressed Kickstart details
+rpbmsi_ResetPC 	EQU 	$04 ;<.l> VEC_RESETPC value
+rpbmsi_SIZEOF  	EQU 	$08
+; struct BootMenuInfo
+rpbmi_SlotCount	EQU 	$00 ;<.b> 1-31 (4MB = 7 slots)
+rpbmi_BootSlot 	EQU 	$01 ;<.b> see rpfwi_BootSlot
+rpbmi_BootConf 	EQU 	$02 ;<.w> ;TODO: bootmenu UI and/or control flags
+rpbmi_SlotValid	EQU 	$04 ;<.l> bit 0 reserved, bit 1-31 slot valid
+rpbmi_SlotInfo 	EQU 	$08 ; struct BootMenuSlotInfo[31] (16MB = 31 slots)
+rpbmi_SIZEOF   	EQU 	(31*rpbmsi_SIZEOF+rpbmi_SlotInfo) ; RPBM_PAGE_SIZE
+
+;-----------------------------------------------------------------------------
+;
+;	RPBM_CMDID_SLOT_TO_KICK
+;	param: <slot_index> (0 = firmware/config)
+;
+; The firmware copies a slot from Flash storage into Kickstart SRAM memory.
+; - firmware sets  rpfwi_KickSlot = <slot_index>
+;
+RPBM_FIRMWARE_TO_KICK	EQU 	0 ; including config storage (BootSlot)
+
+;-----------------------------------------------------------------------------
+;
+;	RPBM_CMDID_JUMP_TO_KICK
+;	param: reserved (0)
+;
+; The firmware exits bootmenu mode and switches to Kickstart SRAM memory.
+; Point of no return -- the bootmenu is responsible to setup/prepare the
+; system before sending this command, and has to immediately jump to the
+; Kickstart (jump has to be run from the CPU instruction prefetch queue).
+;
+RPBM_JUMP_TO_KICK_PARAM	EQU 	0
+
+	ENDC
